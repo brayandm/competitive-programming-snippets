8e05e7efe17abdb677751ec0bc912425980a3bd6
Brayan Durán Medina <brayanduranmedina@gmail.com>
2025-02-20 16:36:37 +0100

    Revert "Refactor snippet generation functions for improved readability and efficiency"
    This reverts commit 874ca9ca3aeff1e25b47b5c2d8e352581a97e17a.

--- generateSnippets.py
+++ generateSnippets.py
@@ -1,55 +1,100 @@
 import os
-import shutil
 import xml.etree.ElementTree as ET
 import re
 
 
 def generate_vscode_snippets():
-    def get_dir_snippets_with_extension(directory, extension):
+
+    def get_dir_snippets_with_extension(dir, extension):
+
+        paths = os.walk(dir)
+
         good_paths = []
-        for root, _, files in os.walk(directory):
-            for file in files:
-                if file.endswith(extension):
-                    good_paths.append(os.path.join(root, file))
+
+        for path in paths:
+
+            for file in path[2]:
+
+                if len(file) >= len(extension) and file[-len(extension) :] == extension:
+
+                    good_paths.append(os.path.join(path[0], file))
+
         return good_paths
 
     def delete_starting_and_ending_lines(lines):
-        new_lines = lines[:]
-        while new_lines and new_lines[0] == "\n":
+
+        new_lines = lines.copy()
+
+        while new_lines[0] == "\n":
+
             new_lines = new_lines[1:]
-        while new_lines and new_lines[-1] == "\n":
+
+        while new_lines[-1] == "\n":
+
             new_lines = new_lines[:-1]
+
         return new_lines
 
     def get_code(lines):
+
         lines = delete_starting_and_ending_lines(lines)[2:-6]
 
         for i in range(len(lines)):
-            lines[i] = lines[i].rstrip()
-            lines[i] = lines[i].replace('"', '\\"')
-            lines[i] = lines[i].replace("'", "\\'")
-            lines[i] = lines[i].replace("\t", "\\t")
-            lines[i] = lines[i].replace("\\", "\\\\")
+
+            lines[i] = lines[i][:-1]
+
+            for j in reversed(range(len(lines[i]))):
+
+                if lines[i][j] == '"':
+
+                    lines[i] = lines[i][:j] + '\\"' + lines[i][j + 1 :]
+
+                elif lines[i][j] == "'":
+
+                    lines[i] = lines[i][:j] + "\\'" + lines[i][j + 1 :]
+
+                elif lines[i][j] == "\t":
+
+                    lines[i] = lines[i][:j] + "\\\t" + lines[i][j + 1 :]
+
+                elif lines[i][j] == "\\":
+
+                    lines[i] = lines[i][:j] + "\\\\" + lines[i][j + 1 :]
 
         return lines
 
     def get_command(lines):
+
         return delete_starting_and_ending_lines(lines)[-4][16:-14]
 
     def normalize_name(name):
+
         name += "_"
+
         new_name = ""
+
         temp_name = ""
-        for char in name:
-            if char != "_":
-                temp_name += char
+
+        for i in range(len(name)):
+
+            if name[i] != "_":
+
+                temp_name += name[i]
+
             else:
-                if temp_name:
-                    if not new_name:
-                        new_name = temp_name.capitalize()
+
+                if len(temp_name) > 0:
+
+                    if len(new_name) == 0:
+
+                        new_name += temp_name.capitalize()
+
                     else:
+
                         new_name += " " + temp_name.capitalize()
+
                     temp_name = ""
+
         return new_name
 
     extension = ".sublime-snippet"
@@ -59,41 +104,51 @@ def generate_vscode_snippets():
     new_folder = "VSCode-snippets"
 
     if os.path.exists(new_folder):
-        shutil.rmtree(new_folder)
-    shutil.copytree(folder, new_folder)
+        os.system(f"rm -r {new_folder}")
+    os.system("cp -r " + folder + " " + new_folder)
 
-    paths = get_dir_snippets_with_extension(new_folder, extension)
+    path = get_dir_snippets_with_extension(new_folder, extension)
 
-    for file_path in paths:
-        with open(file_path, "r", encoding="utf-8") as file:
-            lines = file.readlines()
+    for file_path in path:
+
+        file = open(file_path, "r")
+
+        lines = file.readlines()
+
+        file.close()
 
         code = get_code(lines)
+
         command = get_command(lines)
+
         name = normalize_name(command)
 
-        text = """
-{
-    "%s": {
-        "scope": "cpp",
-        "prefix": "%s",
-        "body": [
-%s        ],
-        "description": "%s"
-    }
-}
-""" % (
-            name,
-            command,
-            "\n".join(f'        "{line}",' for line in code).rstrip(","),
-            name,
-        )
+        text = ""
+
+        text += "{\n"
+        text += '\t"' + name + '": {\n'
+        text += '\t\t"scope": "cpp",\n'
+        text += '\t\t"prefix": "' + command + '",\n'
+        text += '\t\t"body": [\n'
+
+        for line in code:
+
+            text += '\t\t\t"' + line + '",\n'
+
+        text += "\t\t],\n"
+        text += '\t\t"description": "' + name + '"\n'
+        text += "\t}\n"
+        text += "}\n"
 
         os.remove(file_path)
-        new_file_path = file_path[: -len(extension)] + new_extension
-        with open(new_file_path, "w", encoding="utf-8") as file:
-            file.write(text)
-        print(f"Converted {file_path} -> {new_file_path}")
+
+        file = open(file_path[: -len(extension)] + new_extension, "w")
+
+        print(file_path[: -len(extension)] + new_extension)
+
+        file.write(text)
+
+        file.close()
 
 
 def generate_sublime_snippets():
@@ -122,7 +177,10 @@ def generate_sublime_snippets():
     <content><![CDATA[
 {content}
 ]]></content>
+    <!-- Optional: Set a tabTrigger to define how to trigger the snippet -->
     <tabTrigger>{tab_trigger}</tabTrigger>
+    <!-- Optional: Set a scope to limit where the snippet will trigger -->
+    <!-- <scope>source.python</scope> -->
 </snippet>"""
 
             new_file_path = file_path.replace(".cpp", ".sublime-snippet")
@@ -145,8 +203,8 @@ def generate_sublime_snippets():
     target_dir = "Sublime-snippets"
 
     if os.path.exists(target_dir):
-        shutil.rmtree(target_dir)
-    shutil.copytree(source_dir, target_dir)
+        os.system(f"rm -r {target_dir}")
+    os.system(f"cp -r {source_dir} {target_dir}")
 
     snippets_dir = "Sublime-snippets"
     if os.path.exists(snippets_dir):
